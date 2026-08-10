72718a84d6a74137930c5a6140958a14baf47f8b
michellins <mm.dnlins1@gmail.com>
2026-08-10 17:25:21 -0300

    fix(android): remove unused GoogleAuth reference and update versionCode to 2

--- android/app/src/main/java/com/linsora/app/MainActivity.java
+++ android/app/src/main/java/com/linsora/app/MainActivity.java
@@ -1,12 +1,6 @@
 package com.linsora.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        registerPlugin(com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
+
